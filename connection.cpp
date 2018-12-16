d61a717498c2c54258f6e9577569dceb45de08be
Mohammad Kashem <mohammad.kashem@outlook.co.nz>
2018-12-17 12:00:39 +1300

    connecion imp

---
+++ connection.cpp
@@ -0,0 +1,56 @@
+#include "connection.h"
+
+connection::connection(boost::asio::io_service & io_service) :
+	m_io_service(io_service),
+	m_client_socket(io_service), m_buffer{}
+{
+}
+
+connection::~connection()
+= default;
+
+boost::asio::ip::tcp::socket * connection::create_socket()
+{
+	return &m_client_socket;
+}
+
+void connection::read_from_client(const boost::system::error_code & error, size_t bytes_transferred)
+{
+	if(!error)
+	{
+		m_buffer[bytes_transferred] = 0;
+		message_factory->read_message(m_buffer);
+		read();
+	} else
+	{
+		m_client_socket.close();
+	}
+}
+
+void connection::read()
+{
+	m_client_socket.async_read_some(boost::asio::buffer(m_buffer, sizeof(m_buffer)),
+		boost::bind(&connection::read_from_client, 
+		this, 
+		boost::asio::placeholders::error, 
+		boost::asio::placeholders::bytes_transferred));
+}
+
+void connection::send_to_client(const boost::system::error_code & error, size_t bytes_transferred)
+{
+	//TODO: error logging
+}
+
+void connection::write(unsigned char * message)
+{
+	m_client_socket.async_write_some(boost::asio::buffer(message, std::strlen(reinterpret_cast<char*>(message))), 
+		boost::bind(&connection::send_to_client,
+		this,
+		boost::asio::placeholders::error,
+		boost::asio::placeholders::bytes_transferred));
+}
+
+void connection::close()
+{
+	m_client_socket.close();
+}
---
+++ connection.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include "message_factory.h"
+
+class connection
+{
+public:
+	explicit connection(boost::asio::io_service& io_service);
+	virtual ~connection();
+	boost::asio::ip::tcp::socket m_client_socket;
+	boost::asio::ip::tcp::socket* create_socket();
+	void read_from_client(const boost::system::error_code& error, size_t bytes_transferred);
+	void read();
+	void send_to_client(const boost::system::error_code& error, size_t bytes_transferred);
+	void write(unsigned char *message);
+	unsigned char m_buffer[1024];
+	boost::asio::io_service& m_io_service;
+	message_factory *message_factory;
+	void close();
+};
